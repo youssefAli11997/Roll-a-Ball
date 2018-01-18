1ee0aea1a9a8c3a535f9b60d6e1c2e642e829b88
Youssef Ali <youssefali6212@gmail.com>
2018-01-19 00:50:27 +0200

    Uploading the game project

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    private int pickUpsNumber;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        pickUpsNumber = 10;
+        SetCountText();
+        winText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if(count >= pickUpsNumber)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
